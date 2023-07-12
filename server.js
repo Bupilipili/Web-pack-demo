88f8194b1cca501e8a0ef4b2ac5c803d81250ec0
Bupilipili <124571445+Bupilipili@users.noreply.github.com>
2023-07-12 12:25:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 
 const app = express();
-
 const config = require('./webpack.config.js');
+
 const compiler = webpack(config);
 
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
